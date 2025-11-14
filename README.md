98d91c593c5b01a528b9361a2b09319ea5c16a6a
Crhonopost <nath.souvignet@gmail.com>
2025-11-14 12:09:10 +0100

    fixes on installation + begin tutorial

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
+# Steps for setup
 
+matplotlib may cause problems, install it with
+```shell
+pip install matplotlib
+```
+
+## Linux
 ```shell
 cd code
 python -m venv .venv/
 source .venv/bin/activate
-pip install -r requirements.txt --index-url https://download.pytorch.org/whl/cu126
+pip install -r requirements.txt
 
 mkdir data
 mkdir models
 ```
+
+### Old install command (don't use)
+```shell
+pip install -r requirements.txt --index-url https://download.pytorch.org/whl/cu126
+```
+
+
+## Windows
+```shell
+cd code
+python -m venv .venv/
+.venv\Scripts\activate
+pip install -r requirements.txt
+
+mkdir data
+mkdir models
+```
+
+
+
+Tuto MAE: https://medium.com/@ovularslan/masked-autoencoders-mae-the-art-of-seeing-more-by-masking-most-pytorch-implementation-4566e08c66a6
---
+++ code/scripts/tuto-DCGAN.py
@@ -0,0 +1,72 @@
+# https://docs.pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+import torch
+import torch.nn as nn
+import numpy as np
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+# Root directory for dataset
+dataroot = "../data/celeba"
+
+# Number of workers for dataloader
+workers = 2
+
+# Batch size during training
+batch_size = 128
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size = 64
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+nz = 100
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 64
+
+# Number of training epochs
+num_epochs = 5
+
+# Learning rate for optimizers
+lr = 0.0002
+
+# Beta1 hyperparameter for Adam optimizers
+beta1 = 0.5
+
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
+
+
+
+
+
+
+# Create the dataset
+dataset = datasets.ImageFolder(root=dataroot,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+# Create the dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=workers)
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# Plot some training images
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.show()
